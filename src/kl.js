b964955fe51c4feeb3bc7b0d96d5a30d006c9b2f
Robert Koeninger <rkoeninger@att.net>
2017-11-12 23:28:53 -0500

    Made klambda polyfill part of main bundle

--- src/kl.js
+++ src/kl.js
@@ -165,6 +165,19 @@ kl.defun('close', 1, s => err('not implemented'));
 kl.defun('read-byte', 1, s => err('not implemented'));
 kl.defun('write-byte', 2, (s, b) => err('not implemented'));
 
+setTimeout(function () {
+    for (let script of document.scripts) {
+        if (script.type.toLowerCase() !== 'text/klambda') continue;
+        if (script.executed) continue;
+        script.executed = true;
+        if (script.text) {
+            Parser.parseAllString(script.text).map(Transpiler.translateHead).map(eval);
+            continue;
+        }
+        console.warn('klambda script tags must have embedded code');
+    }
+}, 0);
+
 if (typeof module !== 'undefined') {
     module.exports = { Kl, kl };
 }

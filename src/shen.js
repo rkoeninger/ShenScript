1bc65e179fd43ef355b627850af474921ad5a7f1
Robert Koeninger <rkoeninger@att.net>
2019-01-28 18:38:42 -0500

    Simplified symbol, function maps, added form length checking
    Added js<->shen bool conversions

--- src/shen.js
+++ src/shen.js
@@ -27,19 +27,8 @@ const butLast = a => [a.slice(0, -1), a[a.length - 1]];
 const comp = (f, g) => x => f(g(x));
 const raise = x => { throw new Error(x); };
 
-const symbolMap = new Map();
-const functionMap = new Map();
-const getOrRaise = (map, id, adjective) => map.has(id) ? map.get(id) : raise(`${adjective} ${show(id)} not defined`);
-
-export const symbols = {
-  get: id => getOrRaise(symbolMap, id, 'Symbol'),
-  set: (id, value) => (symbolMap.set(id, value), value)
-};
-
-export const functions = {
-  get: id => getOrRaise(functionMap, id, 'Function'),
-  set: (id, parameters, body) => (functionMap.set(id, new Function(...[...parameters, body])), id)
-};
+export const intern = name => Symbol.for(name);
+export const nameOf = symbol => Symbol.keyFor(symbol);
 
 const Cons = class {
   constructor(head, tail) {
@@ -59,27 +48,35 @@ export const isError    = x => x instanceof Error;
 export const isCons     = x => x instanceof Cons;
 export const isStream   = x => false; // TODO: implement this
 
-const asNumber = x => isNumber(x) ? x : raise('Number expected');
-const asString = x => isString(x) ? x : raise('String expected');
-const asSymbol = x => isSymbol(x) ? x : raise('Symbol expected');
-const asArray  = x => isArray(x)  ? x : raise('Array expected');
-const asCons   = x => isCons(x)   ? x : raise('Cons expected');
-const asError  = x => isError(x)  ? x : raise('Error expected');
-const asStream = x => isStream(x) ? x : raise('Stream expected');
+const asNumber = x => isNumber(x) ? x : raise('number expected');
+const asString = x => isString(x) ? x : raise('string expected');
+const asSymbol = x => isSymbol(x) ? x : raise('symbol expected');
+const asArray  = x => isArray(x)  ? x : raise('array expected');
+const asCons   = x => isCons(x)   ? x : raise('cons expected');
+const asError  = x => isError(x)  ? x : raise('error expected');
+const asStream = x => isStream(x) ? x : raise('stream expected');
 const asIndex  = (i, a) =>
   !Natural.isInteger(i)  ? raise(`index ${i} is not valid`) :
   i < 0 || i >= a.length ? raise(`index ${i} is not with bounds of array length ${a.length}`) :
   i;
+
+const trueSymbol = intern('true');
+const falseSymbol = intern('false');
+
+// TODO: general js<->shen data structure conversion
+
+const asJsBool = x =>
+  x === trueSymbol  ? true :
+  x === falseSymbol ? false :
+  raise(`value ${x} is not a valid boolean`);
+const asShenBool = x => x ? trueSymbol : falseSymbol;
 
 export const head = c => c.head;
 export const tail = c => c.tail;
 export const cons = (h, t) => new Cons(h, t);
 export const consFromArray = a => a.reduceRight((t, h) => cons(h, t), null);
 export const consToArray = c => produce(isCons, head, tail, c);
-export const consToArrayTree = c => produce(isCons, x => isCons(x.head) ? consToArrayTree(x.head) : x.head, tail, c);
-
-export const intern = name => Symbol.for(name);
-export const nameOf = symbol => Symbol.keyFor(symbol);
+export const consToArrayTree = c => produce(isCons, comp(x => isCons(x) ? consToArrayTree(x) : x, head), tail, c);
 
 export const equate = (x, y) =>
   x === y
@@ -121,6 +118,8 @@ const runTrampoline = async x => isTrampoline(x) ? runTrampoline(await x.promise
 
 
 
+const validId = /[a-zA-Z_$][0-9a-zA-Z_$]*/;
+
 // NOTE:
 // context.statement  // if location in target context can be a statement
 // context.expression // if location in target context must be an expression
@@ -146,7 +145,9 @@ const arrow = (params, body, expression = true) => ({ type: 'ArrowFunctionExpres
 
 const but = (x, name, value) => ({ ...x, [name]: value });
 
-const isForm = (expr, lead, length) => (!length || expr.length === length) && expr[0] === intern(lead);
+const isForm = (expr, lead, length) =>
+  (!length || expr.length === length || raise(`${lead} must have ${length - 1} argument forms`))
+  && expr[0] === intern(lead);
 const flattenForm = (expr, lead) => isForm(expr, lead) ? expr.slice(1).flatMap(flattenForm) : [expr];
 const flattenLogicalForm = (context, expr, lead) =>
   flattenForm(expr, lead)
@@ -164,95 +165,112 @@ const isReferenced = (symbol, expr) =>
 // TODO: inlining, type-inferred optimizations
 
 const build = (context, expr) =>
-  isNull(expr) ? literal(null) :
-  isNumber(expr) || isString(expr) ? literal(expr) :
+  isNull(expr) || isNumber(expr) || isString(expr) ? literal(expr) :
   isSymbol(expr) ? (
     context.locals.has(expr)
       ? identifier(nameOf(expr))
       : invoke(identifier('intern'), [literal(nameOf(expr))])) :
-  isForm(expr, 'and') ? flattenLogicalForm(context, expr, 'and') :
-  isForm(expr, 'or')  ? flattenLogicalForm(context, expr, 'or') :
-  isForm(expr, 'if', 4) ?
-    conditional(
-      context.statement,
-      build(but(context, 'kind', 'boolean'), expr[1]),
-      build(context, expr[2]),
-      build(context, expr[3])) :
-  isForm(expr, 'cond') ?
-    // TODO: if cond is in return/expression position, wrap in ReturnStatement
-    expr.slice(1).reduceRight(
-      (chain, [test, consequent]) =>
-        test === intern('true')  ? build(context, consequent) :
-        test === intern('false') ? chain :
-        conditional(
-          context.statement,
-          build(but(context, 'kind', 'boolean'), test),
-          build(context, consequent),
-          chain),
-      invoke(identifier('raise'), [literal('no condition was true')])) :
-  isForm(expr, 'let', 4) ? (
-    // TODO:
-    // in a statement context, we can just add a declaration, maybe surround in a block
-    // in an expression context, we might have to put it in an ifee, or attempt inlining
-    // binding may also be ignored in subsequent context, if so, just turn it into a do
-    nameOf(expr[1]) === '_' || !isReferenced(expr[1], expr[3])
-      ? build(context, [intern('do'), expr[2], expr[3]])
-      : statement(invoke(arrow([literal(nameOf(expr[1]))], build(context, expr[2])), [build(context, expr[3])]))) :
-  isForm(expr, 'do') ? body(flattenForm(expr, 'do').map(x => build(context, x))) :
-  // TODO: return butlast(exprs)
-  // if do is assigned to a variable, just make last line an assignment to a variable
-  isForm(expr, 'lambda', 3) ? arrow([identifier(nameOf(expr[1]))], build(context, expr[2])) :
-  // TODO: check body to see if function should be a statement or expression lambda
-  isForm(expr, 'freeze', 2) ? arrow([], build(context, expr[1])) :
-  isForm(expr, 'trap-error', 3) ? attempt(build(context, expr[1]), identifier('E'), block([])) :
-  // TODO: wrap in block statement, make it statement context
-  // TODO: if handler is a lambda, just inline its body, name catch var after lambda param
-  // TODO: apply handler expr[2] to E
-  raise('not a valid form');
-  // TODO: application form
+  isArray(expr) ? (
+    isForm(expr, 'and') ? flattenLogicalForm(context, expr, 'and') :
+    isForm(expr, 'or')  ? flattenLogicalForm(context, expr, 'or') :
+    isForm(expr, 'if', 4) ?
+      conditional(
+        context.statement,
+        build(but(context, 'kind', 'boolean'), expr[1]),
+        build(context, expr[2]),
+        build(context, expr[3])) :
+    isForm(expr, 'cond') ?
+      // TODO: if cond is in return/expression position, wrap in ReturnStatement
+      expr.slice(1).reduceRight(
+        (chain, [test, consequent]) =>
+          test === intern('true')  ? build(context, consequent) :
+          test === intern('false') ? chain :
+          conditional(
+            context.statement,
+            build(but(context, 'kind', 'boolean'), test),
+            build(context, consequent),
+            chain),
+        invoke(identifier('raise'), [literal('no condition was true')])) :
+    isForm(expr, 'let', 4) ? (
+      // TODO:
+      // in a statement context, we can just add a declaration, maybe surround in a block
+      // in an expression context, we might have to put it in an ifee, or attempt inlining
+      // binding may also be ignored in subsequent context, if so, just turn it into a do
+      nameOf(expr[1]) === '_' || !isReferenced(expr[1], expr[3])
+        ? build(context, [intern('do'), expr[2], expr[3]])
+        : statement(invoke(arrow([literal(nameOf(expr[1]))], build(context, expr[2])), [build(context, expr[3])]))) :
+    isForm(expr, 'do') ? body(flattenForm(expr, 'do').map(x => build(context, x))) :
+    // TODO: return butlast(exprs)
+    // if do is assigned to a variable, just make last line an assignment to a variable
+    isForm(expr, 'lambda', 3) ? arrow([identifier(nameOf(expr[1]))], build(context, expr[2])) :
+    // TODO: check body to see if function should be a statement or expression lambda
+    isForm(expr, 'freeze', 2) ? arrow([], build(context, expr[1])) :
+    isForm(expr, 'trap-error', 3) ? attempt(build(context, expr[1]), identifier('E'), block([])) :
+    // TODO: wrap in block statement, make it statement context
+    // TODO: if handler is a lambda, just inline its body, name catch var after lambda param
+    // TODO: apply handler expr[2] to E
+    isForm(expr, 'defun', 4) ? (functions.set(expr[1], expr[2], build(context, expr[3])), expr[1]) :
+    // TODO: set params as locals, set root function context
+    null // TODO: application form
+  ) : raise('not a valid form');
+
   // TODO: what about non-empty, non-number, non-string, non-symbol, non-cons values
 
-export const transpile = expr => build({ locals: new Set() }, consToArrayTree(expr));
+export const transpile = expr => build({ locals: new Set(), head: true }, consToArrayTree(expr));
 
-exports['number?']         = isNumber;
-exports['string?']         = isString;
-exports['symbol?']         = isSymbol;
-exports['absvector?']      = isArray;
-exports['cons?']           = isCons;
-exports['hd']              = c => head(asCons(c));
-exports['tl']              = c => tail(asCons(c));
-exports['cons']            = cons;
-exports['tlstr']           = s => asString(s).substring(1);
-exports['cn']              = (s, t) => asString(s) + asString(t);
-exports['string->n']       = s => asString(s).charCodeAt(0);
-exports['n->string']       = n => String.fromCharCode(asNumber(n));
-exports['pos']             = (s, i) => asString(s)[asNumber(i)];
-exports['str']             = show;
-exports['absvector']       = n => new Array(asNumber(n)).fill(null);
-exports['<-absvector']     = (a, i) => asArray(a)[asIndex(i, a)];
-exports['absvector->']     = (a, i, x) => (asArray(a)[asIndex(i, a)] = x, a);
-exports['=']               = equate;
-exports['+']               = (x, y) => asNumber(x) +  asNumber(y);
-exports['-']               = (x, y) => asNumber(x) -  asNumber(y);
-exports['*']               = (x, y) => asNumber(x) *  asNumber(y);
-exports['/']               = (x, y) => asNumber(x) /  asNumber(y);
-exports['>']               = (x, y) => asNumber(x) >  asNumber(y);
-exports['<']               = (x, y) => asNumber(x) <  asNumber(y);
-exports['>=']              = (x, y) => asNumber(x) >= asNumber(y);
-exports['<=']              = (x, y) => asNumber(x) <= asNumber(y);
-exports['intern']          = s => intern(asString(s));
-exports['get-time']        = s => getTime(asSymbol(s));
-exports['type']            = (x, _) => x;
-exports['simple-error']    = raise;
-exports['error-to-string'] = x => asError(x).message;
-exports['if']              = (c, x, y) => c ? x : y; // TODO: convert to js bool
-exports['and']             = (x, y) => x && y; // TODO: convert to js bool
-exports['or']              = (x, y) => x || y; // TODO: convert to js bool
-exports['set']             = set;
-exports['value']           = value;
-exports['open']            = undefined; // TODO: define these
-exports['close']           = undefined; // TODO: define these
-exports['read-byte']       = undefined; // TODO: define these
-exports['write-byte']      = undefined; // TODO: define these
+const globalMap = adjective => {
+  const map = {};
+  return {
+    get: id => {
+      const value = map[id];
+      return value !== undefined ? value : raise(`${adjective} ${id} not defined`);
+    },
+    set: (id, value) => map[id] = value
+  };
+};
+
+export const symbols = globalMap('symbol');
+export const functions = globalMap('function');
+
+functions['number?']         = isNumber;
+functions['string?']         = isString;
+functions['symbol?']         = isSymbol;
+functions['absvector?']      = isArray;
+functions['cons?']           = isCons;
+functions['hd']              = comp(head, asCons);
+functions['tl']              = comp(tail, asCons);
+functions['cons']            = cons;
+functions['tlstr']           = s => asString(s).substring(1);
+functions['cn']              = (s, t) => asString(s) + asString(t);
+functions['string->n']       = s => asString(s).charCodeAt(0);
+functions['n->string']       = comp(String.fromCharCode, asNumber);
+functions['pos']             = (s, i) => asString(s)[asNumber(i)];
+functions['str']             = show;
+functions['absvector']       = n => new Array(asNumber(n)).fill(null);
+functions['<-absvector']     = (a, i) => asArray(a)[asIndex(i, a)];
+functions['absvector->']     = (a, i, x) => (asArray(a)[asIndex(i, a)] = x, a);
+functions['=']               = equate;
+functions['+']               = (x, y) => asNumber(x) +  asNumber(y);
+functions['-']               = (x, y) => asNumber(x) -  asNumber(y);
+functions['*']               = (x, y) => asNumber(x) *  asNumber(y);
+functions['/']               = (x, y) => asNumber(x) /  asNumber(y);
+functions['>']               = (x, y) => asNumber(x) >  asNumber(y);
+functions['<']               = (x, y) => asNumber(x) <  asNumber(y);
+functions['>=']              = (x, y) => asNumber(x) >= asNumber(y);
+functions['<=']              = (x, y) => asNumber(x) <= asNumber(y);
+functions['intern']          = comp(intern, asString);
+functions['get-time']        = comp(getTime, asSymbol);
+functions['type']            = (x, _) => x;
+functions['simple-error']    = raise;
+functions['error-to-string'] = comp(asError, x => x.message);
+functions['if']              = (c, x, y) => c ? x : y; // TODO: convert to js bool
+functions['and']             = (x, y) => x && y; // TODO: convert to js bool
+functions['or']              = (x, y) => x || y; // TODO: convert to js bool
+functions['set']             = set;
+functions['value']           = value;
+functions['open']            = undefined; // TODO: define these
+functions['close']           = undefined; // TODO: define these
+functions['read-byte']       = undefined; // TODO: define these
+functions['write-byte']      = undefined; // TODO: define these
 
 // TODO: IO operations, file system, console

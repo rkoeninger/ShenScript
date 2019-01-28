4f44f528b31f7200d1700f60182ca6b7682f018d
Robert Koeninger <rkoeninger@att.net>
2019-01-27 23:49:16 -0500

    Improved parser

--- parser.js
+++ parser.js
@@ -1,11 +1,11 @@
 const { alt, createLanguage, optWhitespace, regexp, string } = require('parsimmon');
 
 const Whitespace = () => optWhitespace;
-const Numeric = () => regexp(/[0-9]+/).map(Number);
+const Numeric = () => regexp(/[+-]?[0-9]+(\.[0-9]+)?/).map(Number);
 const CharSeq = () => regexp(/"[^"]*"/);
-const Symbolic = () => regexp(/[a-z]+/).map(Symbol.for);
+const Symbolic = () => regexp(/[^\s]+/).map(Symbol.for);
 const Form = r => string('(').then(r.Value.sepBy(r.Whitespace)).skip(string(')'));
 const Value = r => alt(r.Numeric, r.CharSeq, r.Symbolic, r.Form);
-const lang = createLanguage({ Numeric, CharSeq, Symbolic, Form, Value, Whitespace });
+const lang = createLanguage({ Whitespace, Numeric, CharSeq, Symbolic, Form, Value });
 
 export const parse = syntax => lang.Value.tryParse(syntax);

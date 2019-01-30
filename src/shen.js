012c957160d9f2f8b974fa04618a6323c8905311
Robert Koeninger <rkoeninger@att.net>
2019-01-30 18:36:03 -0500

    Added Tube, Source, Sink
    Fixed flattenForm
    Added ensure, converters
    Added build for type function

--- src/shen.js
+++ src/shen.js
@@ -5,12 +5,12 @@
  * Empty        null
  * Number       number
  * String       string
- * Symbol       Symbol
+ * Symbol       symbol
  * Function     function
  * AbsVector    array
  * Error        Error
  * Cons         Cons
- * Stream       TODO: ??????????????
+ * Stream       Tube
  */
 
 const produce = (proceed, render, next, state) => {
@@ -22,7 +22,6 @@ const produce = (proceed, render, next, state) => {
   return array;
 };
 
-const lastOf = (...xs) => xs.length === 0 ? null : xs[xs.length - 1];
 const butLast = a => [a.slice(0, -1), a[a.length - 1]];
 const comp = (f, g) => x => f(g(x));
 const raise = x => { throw new Error(x); };
@@ -37,16 +36,87 @@ const Cons = class {
   }
 };
 
+// TODO: Tube, Source, Sink are browser-only?
+
+const Stream = class {
+  constructor(name) {
+    this.name = name;
+    this.closed = false;
+  }
+  close() {
+    this.closed = true;
+  }
+  checkClosed() {
+    if (this.closed) {
+      raise(this.closedMessage);
+    }
+  }
+  get closedMessage() {
+    return `${this.constructor.name} ${this.name} has been closed`;
+  }
+}
+
+const Source = class extends Tube {
+  constructor(name) {
+    super(name);
+    this.closed = false;
+    this.buffer = [];
+    this.queue = [];
+  }
+  read() {
+    this.checkClosed();
+    if (this.buffer.length > 0) {
+      return this.buffer.shift();
+    } else {
+      return new Promise((resolve, reject) => {
+        this.queue.push({ resolve, reject });
+      }).then(() => this.read());
+    }
+  }
+  supply(bytes) {
+    this.checkClosed();
+    this.buffer.push(...bytes);
+    if (this.queue.length > 0) {
+      this.queue.shift().resolve();
+    }
+  }
+  close() {
+    super.close();
+    for (const receiver of queue) {
+      receiver.reject(this.closedMessage);
+    }
+    return null;
+  }
+};
+
+const Sink = class extends Tube {
+  constructor(name, receiver) {
+    this.name = name;
+    this.receiver = receiver;
+  }
+  write(byte) {
+    this.checkClosed();
+    this.receiver(byte);
+    return null;
+  }
+};
+
+// TODO: this should be supplied when node/browser packaging is done
+export const terminal = {
+  in: new Source('terminal'),
+  out: new Sink('terminal', () => {}),
+  error: new Sink('terminal', () => {})
+};
+
 export const isNull     = x => x === null;
 export const isNumber   = x => isFinite(x);
 export const isString   = x => typeof x === 'string';
 export const isSymbol   = x => typeof x === 'symbol';
-export const isAtom     = x => isNumber(x) || isString(x) || isSymbol(x);
 export const isFunction = x => typeof x === 'function';
 export const isArray    = x => typeof x === 'array';
 export const isError    = x => x instanceof Error;
 export const isCons     = x => x instanceof Cons;
-export const isStream   = x => false; // TODO: implement this
+export const isStream   = x => x instanceof Tube;
 
 const asNumber = x => isNumber(x) ? x : raise('number expected');
 const asString = x => isString(x) ? x : raise('string expected');
@@ -63,7 +133,7 @@ const asIndex  = (i, a) =>
 const trueSymbol = intern('true');
 const falseSymbol = intern('false');
 
-// TODO: general js<->shen data structure conversion
+// TODO: general, recursive js<->shen data structure conversion
 
 const asJsBool = x =>
   x === trueSymbol  ? true :
@@ -91,7 +161,7 @@ export const show = x =>
   isFunction(x) ? `<Function ${x.name}>` :
   isArray(x)    ? `<Vector ${x.length}>` :
   isError(x)    ? `<Error "${x.message}">` :
-  isStream(x)   ? `<Stream>` :
+  isStream(x)   ? `<Stream ${x.name}>` :
   `${x}`;
 
 const now = () => new Date().getTime();
@@ -100,8 +170,6 @@ const getTime = mode =>
   mode === intern('unix') ? now() :
   mode === intern('run')  ? now() - startTime :
   raise('get-time only accepts symbols unix or run');
-
-
 
 // TODO: what about async/await?
 // how to combine trampoline evaluation with promise chaining?
@@ -115,8 +183,6 @@ const isTrampoline = x => x instanceof Trampoline;
 const runTrampoline = async x => isTrampoline(x) ? runTrampoline(await x.promise) : x;
 
 // TODO: partial applications, curried applications
-
-
 
 const validId = /[a-zA-Z_$][0-9a-zA-Z_$]*/;
 // TODO: still need renaming logic for local variables, parameters
@@ -133,23 +199,36 @@ const validId = /[a-zA-Z_$][0-9a-zA-Z_$]*/;
 // context.kind       // specific type is expected for expression, undefined if unknown
 
 const literal = value => ({ type: 'Literal', value });
+const array = elements => ({ type: 'ArrayExpression', elements });
 const identifier = name => ({ type: 'Identifier', name });
-const invoke = (callee, arguments) => ({ type: 'CallExpression', callee, arguments });
+const wait = argument => ({ type: 'AwaitExpression', argument });
+const invoke = (callee, arguments) => wait({ type: 'CallExpression', callee, arguments });
+// TODO: only wrap invocation in await if in async context
+
 const conditional = (statement, test, consequent, alternative) => ({
   type: statement ? 'IfStatement' : 'ConditionalExpression', test, consequent, alternative
 });
 const logical = (operator, left, right) => ({ type: 'LogicalExpression', operator, left, right });
 const attempt = (block, param, body) => ({ type: 'TryStatement', block, handler: { type: 'CatchClause', param, body } });
-const block = body => ({ type: 'BlockStatement', body });
+const access = (object, property) => ({ type: 'MemberExpression', computed: property.type !== 'Literal', object, property });
+const block = (statement, body) => statement ? { type: 'BlockStatement', body } : access(array(body), literal(body.length - 1));
 const statement = expression => ({ type: 'ExpressionStatement', expression });
-const arrow = (params, body, expression = true) => ({ type: 'ArrowFunctionExpression', expression, params, body });
+const arrow = (params, body, expression = true) => ({ type: 'ArrowFunctionExpression', async: true, expression, params, body });
+// TODO: track async in context, and should async always be there for generated code?
+
+const converters = {
+  'number': 'asNumber',
+  'string': 'asString'
+  // TODO: etc: esp. shen bool vs js bool
+};
+const ensure = (kind, expr) => expr.kind === kind ? expr : invoke(identifier(converters[kind]), [expr]);
 
 const but = (x, name, value) => ({ ...x, [name]: value });
 
 const isForm = (expr, lead, length) =>
   (!length || expr.length === length || raise(`${lead} must have ${length - 1} argument forms`))
   && expr[0] === intern(lead);
-const flattenForm = (expr, lead) => isForm(expr, lead) ? expr.slice(1).flatMap(flattenForm) : [expr];
+const flattenForm = (expr, lead) => isForm(expr, lead) ? expr.slice(1).flatMap(x => flattenForm(x, lead)) : [expr];
 const flattenLogicalForm = (context, expr, lead) =>
   flattenForm(expr, lead)
     .map(x => build(but(context, 'kind', 'boolean'), x)) // TODO: wrap in asJsBool if necessary
@@ -185,8 +264,8 @@ const build = (context, expr) =>
       // TODO: if cond is in return/expression position, wrap in ReturnStatement
       expr.slice(1).reduceRight(
         (chain, [test, consequent]) =>
-          test === intern('true')  ? build(context, consequent) :
-          test === intern('false') ? chain :
+          test === trueSymbol  ? build(context, consequent) :
+          test === falseSymbol ? chain :
           conditional(
             context.statement,
             build(but(context, 'kind', 'boolean'), test),
@@ -197,9 +276,9 @@ const build = (context, expr) =>
       // TODO: in a statement context, we can just add a declaration, maybe surround in a block
       // in an expression context, we might have to put it in an ifee, or attempt inlining
       nameOf(expr[1]) === '_' || !isReferenced(expr[1], expr[3])
-        ? build(context, [intern('do'), expr[2], expr[3]])
+        ? build(context, [intern('do'), expr[2], expr[3]]) // (let _ X Y) => (do X Y)
         : statement(invoke(arrow([literal(nameOf(expr[1]))], build(context, expr[2])), [build(context, expr[3])]))) :
-    isForm(expr, 'do') ? body(flattenForm(expr, 'do').map(x => build(context, x))) :
+    isForm(expr, 'do') ? block(context.statement, flattenForm(expr, 'do').map(x => build(context, x))) :
     // TODO: return butlast(exprs)
     // if do is assigned to a variable, just make last line an assignment to a variable
     isForm(expr, 'lambda', 3) ? arrow([identifier(nameOf(expr[1]))], build(context, expr[2])) :
@@ -211,6 +290,7 @@ const build = (context, expr) =>
     // TODO: apply handler expr[2] to E
     isForm(expr, 'defun', 4) ? (functions.set(expr[1], expr[2], build(context, expr[3])), expr[1]) :
     // TODO: set params as locals, set root function context
+    isForm(expr, 'type', 2) ? expr[1] : // TODO: tag returned expr as having type nameof(expr[2])
     null // TODO: application form
   ) : raise('not a valid form');
 
@@ -259,16 +339,21 @@ functions['<=']              = (x, y) => asNumber(x) <= asNumber(y);
 functions['intern']          = comp(intern, asString);
 functions['get-time']        = comp(getTime, asSymbol);
 functions['type']            = (x, _) => x;
-functions['simple-error']    = raise;
-functions['error-to-string'] = comp(asError, x => x.message);
-functions['if']              = (c, x, y) => c ? x : y; // TODO: convert to js bool
-functions['and']             = (x, y) => x && y; // TODO: convert to js bool
-functions['or']              = (x, y) => x || y; // TODO: convert to js bool
-functions['set']             = set;
-functions['value']           = value;
+functions['simple-error']    = comp(raise, asString);
+functions['error-to-string'] = x => asError(x).message;
+functions['if']              = (c, x, y) => asJsBool(c) ? x : y;
+functions['and']             = (x, y) => asShenBool(asJsBool(x) && asJsBool(y));
+functions['or']              = (x, y) => asShenBool(asJsBool(x) || asJsBool(y));
+functions['set']             = (s, x) => symbols.set(nameOf(asSymbol(s)), x);
+functions['value']           = s => symbols.get(nameOf(asSymbol(s)), x);
+
+// TODO: these get moved to node/browser packaging?
 functions['open']            = undefined; // TODO: define these
-functions['close']           = undefined; // TODO: define these
-functions['read-byte']       = undefined; // TODO: define these
-functions['write-byte']      = undefined; // TODO: define these
+functions['close']           = s => asStream(s).close();
+functions['read-byte']       = s => asInStream(s).read(); // TODO: define asInStream
+functions['write-byte']      = (s, b) => asOutStream(s).write(b); // TODO: define asOutStream
+symbols['*stinput*']         = consoleSource;
+symbols['*stoutput*']        = consoleSink;
+symbols['*sterror*']         = consoleSink;
 
 // TODO: IO operations, file system, console

9b313523cc32164103579a13f51b392c02347c45
Robert Koeninger <rkoeninger@att.net>
2019-02-04 20:13:46 -0500

    Updated to Shen Kernel 21.1

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,7 @@ const srcFiles = ['./src/**/*.js', './dist/kernel.js'];
 const testFiles = './tests.js';
 const distRoot = './dist';
 
-const kernelVersion = '21.0';
+const kernelVersion = '21.1';
 const kernelFolderName = `ShenOSKernel-${kernelVersion}`;
 const kernelArchiveName = `${kernelFolderName}.tar.gz`;
 const kernelArchiveUrlBase = 'https://github.com/Shen-Language/shen-sources/releases/download';

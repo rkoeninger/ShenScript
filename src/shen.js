60daddd3e5e514e8a377bbfa60f9b9bdd1bdf719
Robert Koeninger <rkoeninger@att.net>
2019-02-07 00:06:18 -0500

    Injected isStream, open functions
    Removed dead Tube code

--- src/shen.js
+++ src/shen.js
@@ -10,7 +10,7 @@
  * AbsVector    array
  * Error        Error
  * Cons         Cons
- * Stream       Tube
+ * Stream       *
  */
 
 const produce = (proceed, render, next, state) => {
@@ -35,78 +35,6 @@ const Cons = class {
   }
 };
 
-// TODO: Tube, Source, Sink are browser-only?
-
-const Stream = class {
-  constructor(name) {
-    this.name = name;
-    this.closed = false;
-  }
-  close() {
-    this.closed = true;
-  }
-  checkClosed() {
-    if (this.closed) {
-      raise(this.closedMessage);
-    }
-  }
-  get closedMessage() {
-    return `${this.constructor.name} ${this.name} has been closed`;
-  }
-}
-
-const Source = class extends Tube {
-  constructor(name) {
-    super(name);
-    this.closed = false;
-    this.buffer = [];
-    this.queue = [];
-  }
-  read() {
-    this.checkClosed();
-    if (this.buffer.length > 0) {
-      return this.buffer.shift();
-    } else {
-      return new Promise((resolve, reject) => {
-        this.queue.push({ resolve, reject });
-      }).then(() => this.read());
-    }
-  }
-  supply(bytes) {
-    this.checkClosed();
-    this.buffer.push(...bytes);
-    if (this.queue.length > 0) {
-      this.queue.shift().resolve();
-    }
-  }
-  close() {
-    super.close();
-    for (const receiver of queue) {
-      receiver.reject(this.closedMessage);
-    }
-    return null;
-  }
-};
-
-const Sink = class extends Tube {
-  constructor(name, receiver) {
-    this.name = name;
-    this.receiver = receiver;
-  }
-  write(byte) {
-    this.checkClosed();
-    this.receiver(byte);
-    return null;
-  }
-};
-
-// TODO: this should be supplied when node/browser packaging is done
-export const terminal = {
-  in: new Source('terminal'),
-  out: new Sink('terminal', () => {}),
-  error: new Sink('terminal', () => {})
-};
-
 export const isNull     = x => x === null;
 export const isNumber   = x => isFinite(x);
 export const isString   = x => typeof x === 'string';
@@ -115,7 +43,6 @@ export const isFunction = x => typeof x === 'function';
 export const isArray    = x => typeof x === 'array';
 export const isError    = x => x instanceof Error;
 export const isCons     = x => x instanceof Cons;
-export const isStream   = x => x instanceof Tube;
 
 const asNumber = x => isNumber(x) ? x : raise('number expected');
 const asString = x => isString(x) ? x : raise('string expected');
@@ -123,7 +50,6 @@ const asSymbol = x => isSymbol(x) ? x : raise('symbol expected');
 const asArray  = x => isArray(x)  ? x : raise('array expected');
 const asCons   = x => isCons(x)   ? x : raise('cons expected');
 const asError  = x => isError(x)  ? x : raise('error expected');
-const asStream = x => isStream(x) ? x : raise('stream expected');
 const asIndex  = (i, a) =>
   !Natural.isInteger(i)  ? raise(`index ${i} is not valid`) :
   i < 0 || i >= a.length ? raise(`index ${i} is not with bounds of array length ${a.length}`) :
@@ -160,26 +86,8 @@ export const show = x =>
   isFunction(x) ? `<Function ${x.name}>` :
   isArray(x)    ? `<Vector ${x.length}>` :
   isError(x)    ? `<Error "${x.message}">` :
-  isStream(x)   ? `<Stream ${x.name}>` :
+  isStream(x)   ? `<Stream ${x.name}>` : // TODO: how to access isStream ?
   `${x}`;
-
-const now = () => new Date().getTime();
-const startTime = now();
-const getTime = mode =>
-  mode === intern('unix') ? now() :
-  mode === intern('run')  ? now() - startTime :
-  raise('get-time only accepts symbols unix or run');
-
-const globalMap = adjective => {
-  const map = {};
-  return {
-    get: id => {
-      const value = map[id];
-      return value !== undefined ? value : raise(`${adjective} ${id} not defined`);
-    },
-    set: (id, value) => map[id] = value
-  };
-};
 
 // TODO: what about async/await?
 // how to combine trampoline evaluation with promise chaining?
@@ -265,6 +173,8 @@ const isReferenced = (symbol, expr) =>
      || isForm(expr, 'defun', 4) && !expr[2].includes(symbol) && isReferenced(symbol, expr[3])
      || expr.some(x => isReferenced(symbol, x)));
 
+const accessSymbol = symbol => access(identifier('symbols'), literal(nameOf(symbol)));
+
 // TODO: async/await
 // TODO: inlining, type-inferred optimizations
 
@@ -312,11 +222,20 @@ const build = (context, expr) =>
         : attempt(build(context, expr[1]), identifier('$error'), invoke(build(context, expr[2]), [identifier('$error')]))) :
     // TODO: wrap in block statement, make it statement context
     // TODO: emit functions.set instead of doing it (how to scope it?)
-    isForm(expr, 'defun', 4) ? (functions.set(expr[1], expr[2], build(context, expr[3])), expr[1]) :
+    // TODO: defuns dot not capture local scope
+    isForm(expr, 'defun', 4) ? (
+      invoke(
+        access(literal('functions'), literal('set')),
+        [
+          literal(nameOf(expr[1])),
+          array(expr[2].map(x => identifier(nameOf(x)))),
+          build(context, expr[3])
+        ]),
+      expr[1]) :
     // TODO: set params as locals, set root function context
-    isForm(expr, 'value', 2) ? access(identifier('symbols'), literal(nameOf(expr[1]))) : // TODO: make identifier if valid identifier
+    isForm(expr, 'value', 2) ? accessSymbol(expr[1]) : // TODO: make identifier if valid identifier
     // TODO: extract code for value to use in set
-    isForm(expr, 'set', 3) ? assign(access(identifier('symbols', literal(nameOf(expr[1])))), build(context, expr[2])) :
+    isForm(expr, 'set', 3) ? assign(accessSymbol(expr[1]), build(context, expr[2])) :
     isForm(expr, 'type', 2) ? expr[1] : // TODO: tag returned expr as having type nameof(expr[2])
     null // TODO: application form
   ) : raise('not a valid form');
@@ -326,85 +245,90 @@ export const transpile = expr => build({ locals: new Set(), head: true }, consTo
 
 // TODO: needs createShenEnv function that takes impls of certain IO functions
 //       like fs, terminal, stinput, stoutput
-//       Depending on how library is packaged or deployed, these could work very
-//       differently
+//       Depending on how library is packaged or deployed, these could work very differently
 //       may not need to generate async/await syntax if IO functions don't use promises
 
-const Shen = (opts = {}) => {
-  const symbols = globalMap('symbol');
-  const functions = globalMap('function');
-  const terminal = opts.terminal; // undefined terminal means writing does console.log and reading raises error
-  terminal.in  = terminal.in  || ;
-  terminal.out = terminal.out || ;
-  terminal.err = terminal.err || ;
-  symbols['*stinput*']         = terminal.in;
-  symbols['*stoutput*']        = terminal.out;
-  symbols['*sterror*']         = terminal.err;
-  functions['open']            = undefined; // TODO: define these
-  functions['close']           = s => asStream(s).close();
-  functions['read-byte']       = s => asInStream(s).read(); // TODO: define asInStream
-  functions['write-byte']      = (s, b) => asOutStream(s).write(b); // TODO: define asOutStream
-  functions['number?']         = isNumber;
-  functions['string?']         = isString;
-  functions['symbol?']         = isSymbol;
-  functions['absvector?']      = isArray;
-  functions['cons?']           = isCons;
-  functions['hd']              = c => head(asCons(c));
-  functions['tl']              = c => tail(asCons(c));
-  functions['cons']            = cons;
-  functions['tlstr']           = s => asString(s).substring(1);
-  functions['cn']              = (s, t) => asString(s) + asString(t);
-  functions['string->n']       = s => asString(s).charCodeAt(0);
-  functions['n->string']       = n => String.fromCharCode(asNumber(n));
-  functions['pos']             = (s, i) => asString(s)[asNumber(i)];
-  functions['str']             = show;
-  functions['absvector']       = n => new Array(asNumber(n)).fill(null);
-  functions['<-absvector']     = (a, i) => asArray(a)[asIndex(i, a)];
-  functions['absvector->']     = (a, i, x) => (asArray(a)[asIndex(i, a)] = x, a);
-  functions['=']               = equate;
-  functions['+']               = (x, y) => asNumber(x) +  asNumber(y);
-  functions['-']               = (x, y) => asNumber(x) -  asNumber(y);
-  functions['*']               = (x, y) => asNumber(x) *  asNumber(y);
-  functions['/']               = (x, y) => asNumber(x) /  asNumber(y);
-  functions['>']               = (x, y) => asNumber(x) >  asNumber(y);
-  functions['<']               = (x, y) => asNumber(x) <  asNumber(y);
-  functions['>=']              = (x, y) => asNumber(x) >= asNumber(y);
-  functions['<=']              = (x, y) => asNumber(x) <= asNumber(y);
-  functions['intern']          = s => intern(asString(s));
-  functions['get-time']        = s => getTime(asSymbol(s));
-  functions['type']            = (x, _) => x;
-  functions['simple-error']    = s => raise(asString(s));
-  functions['error-to-string'] = x => asError(x).message;
-  functions['if']              = (b, x, y) => asJsBool(b) ? x : y;
-  functions['and']             = (x, y) => asShenBool(asJsBool(x) && asJsBool(y));
-  functions['or']              = (x, y) => asShenBool(asJsBool(x) || asJsBool(y));
-  functions['set']             = (s, x) => symbols.set(nameOf(asSymbol(s)), x);
-  functions['value']           = s => symbols.get(nameOf(asSymbol(s)), x);
-  const fileSystem = opts.fileSystem; // undefined filesystem means opening files raises error
-  const useAsync = opts.useAsync; // async defaults to false, especially if terminal and filesystem opts are undefined
-  const clock = opts.clock || () => new Date().getDate();
-  const port = opts.port || 'Unknown';
-  const porters = opts.porters || 'Unknown';
-  const version = opts.version || 'Unknown';
-  const implementation = opts.implementation || 'Unknown';
-  const release = opts.release || 'Unknown';
-  const os = opts.os || 'Unknown';
+const kl = (options = {}) => {
+  const asInStream  = (x => options.isInStream(x)  ? x : raise('input stream expected'))  || () => false;
+  const asOutStream = (x => options.isOutStream(x) ? x : raise('output stream expected')) || () => false;
+  const isStream = x => options.isInStream(x) || options.isOutStream(x);
+  const asStream = x => isStream(x) ? x : raise('stream expected');
+  const clock = options.clock || () => new Date().getTime();
+  const startTime = clock();
+  const getTime = mode =>
+    mode === intern('unix') ? clock() :
+    mode === intern('run')  ? clock() - startTime :
+    raise('get-time only accepts symbols unix or run');
+  const openRead  = options.openRead  || () => raise('open(in) not supported');
+  const openWrite = options.openWrite || () => raise('open(out) not supported');
+  const open = (mode, path) =>
+    mode === intern('in')  ? openRead(path) :
+    mode === intern('out') ? openWrite(path) :
+    raise('open only accepts symbols in or out');
+  const symbols = {}; // TODO: raise error when reading undefined symbols
+  symbols['*language*']         = 'JavaScript';
+  symbols['*implementation*']   = options.implementation || 'Unknown';
+  symbols['*release*']          = options.release        || 'Unknown';
+  symbols['*os*']               = options.os             || 'Unknown';
+  symbols['*port*']             = options.port           || 'Unknown';
+  symbols['*porters*']          = options.porters        || 'Unknown';
+  symbols['*stinput*']          = options.stinput  || () => raise('standard input not supported');
+  symbols['*stoutput*']         = options.stoutput || () => raise('standard output not supported');
+  symbols['*sterror*']          = options.sterror  || () => raise('standard error not supported');
+  const functions = {}; // TODO: raise error when reading undefined symbols
+  functions['open']             = open;
+  functions['close']            = s => asStream(s).close();
+  functions['read-byte']        = s => asInStream(s).read();
+  functions['write-byte']       = (s, b) => asOutStream(s).write(b);
+  functions['get-time']         = getTime;
+  functions['number?']          = isNumber;
+  functions['string?']          = isString;
+  functions['symbol?']          = isSymbol;
+  functions['absvector?']       = isArray;
+  functions['cons?']            = isCons;
+  functions['hd']               = c => head(asCons(c));
+  functions['tl']               = c => tail(asCons(c));
+  functions['cons']             = cons;
+  functions['tlstr']            = s => asString(s).substring(1);
+  functions['cn']               = (s, t) => asString(s) + asString(t);
+  functions['string->n']        = s => asString(s).charCodeAt(0);
+  functions['n->string']        = n => String.fromCharCode(asNumber(n));
+  functions['pos']              = (s, i) => asString(s)[asNumber(i)];
+  functions['str']              = show;
+  functions['absvector']        = n => new Array(asNumber(n)).fill(null);
+  functions['<-absvector']      = (a, i) => asArray(a)[asIndex(i, a)];
+  functions['absvector->']      = (a, i, x) => (asArray(a)[asIndex(i, a)] = x, a);
+  functions['=']                = equate;
+  functions['+']                = (x, y) => asNumber(x) +  asNumber(y);
+  functions['-']                = (x, y) => asNumber(x) -  asNumber(y);
+  functions['*']                = (x, y) => asNumber(x) *  asNumber(y);
+  functions['/']                = (x, y) => asNumber(x) /  asNumber(y);
+  functions['>']                = (x, y) => asNumber(x) >  asNumber(y);
+  functions['<']                = (x, y) => asNumber(x) <  asNumber(y);
+  functions['>=']               = (x, y) => asNumber(x) >= asNumber(y);
+  functions['<=']               = (x, y) => asNumber(x) <= asNumber(y);
+  functions['intern']           = s => intern(asString(s));
+  functions['get-time']         = s => getTime(asSymbol(s));
+  functions['type']             = (x, _) => x;
+  functions['simple-error']     = s => raise(asString(s));
+  functions['error-to-string']  = x => asError(x).message;
+  functions['if']               = (b, x, y) => asJsBool(b) ? x : y;
+  functions['and']              = (x, y) => asShenBool(asJsBool(x) && asJsBool(y));
+  functions['or']               = (x, y) => asShenBool(asJsBool(x) || asJsBool(y));
+  functions['set']              = (s, x) => symbols[nameOf(asSymbol(s))] = x;
+  functions['value']            = s => symbols[nameOf(asSymbol(s))];
   return {
     symbols,
     functions,
-    terminal,
-    fileSystem,
-    clock,
-    port,
-    porters,
-    version,
-    implementation,
-    release,
-    os
+    asStream,
+    asInStream,
+    asOutStream,
+    isStream,
+    isInStream,
+    isOutStream
   };
 };
 
-export default Shen;
+export default kl;
 
-// TODO: kl() ... translated code ... shen() ... shen.repl()
-//       no constructors!
+// TODO: kl() ... translated code ... shen() ... shen.repl()       no constructors!

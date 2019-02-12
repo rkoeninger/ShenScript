a4a28d73bd030e57e7d51e392ac59615725cc803
Robert Koeninger <rkoeninger@att.net>
2019-02-12 18:37:54 -0500

    Started implementing head/tail call generation
    Lots of cleanup

--- src/shen.js
+++ src/shen.js
@@ -19,6 +19,7 @@ const produce = (proceed, render, next, state) => {
   return array;
 };
 
+const nop = x => x;
 const raise = x => { throw new Error(x); };
 const nameOf = symbol => Symbol.keyFor(symbol);
 const intern = name => Symbol.for(name);
@@ -51,13 +52,11 @@ const isArray    = x => typeof x === 'array';
 const isError    = x => x instanceof Error;
 const isCons     = x => x instanceof Cons;
 
-const head = c => c.head;
-const tail = c => c.tail;
 const cons = (h, t) => new Cons(h, t);
 const consFromArray = a => a.reduceRight((t, h) => cons(h, t), null);
-const consToArray = c => produce(isCons, head, tail, c);
+const consToArray = c => produce(isCons, c => c.head, c => c.tail, c);
 const valueToArrayTree = x => isCons(head(x)) ? consToArrayTree(head(x)) : head(x);
-const consToArrayTree = c => produce(isCons, valueToArrayTree, tail, c);
+const consToArrayTree = c => produce(isCons, valueToArrayTree, c => c.tail, c);
 
 const bounce = (f, ...args) => new Trampoline(f, ...args);
 const settle = x => {
@@ -87,48 +86,45 @@ const app = (f, args) =>
   f.arity > args.length ? app(f(...args.slice(0, f.arity)), args.slice(f.arity)) :
   func((...more) => app(f, [...args, ...more]), args.length - f.arity);
 
-/* statement    if location in target context can be a statement               *
- * expression   if location in target context must be an expression            *
- * return       location is the last statement which needs to be returned      *
- * assignment   expr is getting assigned to a variable                         *
- * head         if context is in head position                                 *
- * tail         if context is in tail position                                 *
- * locals       Set of local variables and parameters defined at this point    *
- * scopeName    Name of enclosing function/file                                *
- * kind         specific type is expected for expression, undefined if unknown */
-
 const literal = value => ({ type: 'Literal', value });
 const array = elements => ({ type: 'ArrayExpression', elements });
 const identifier = name => ({ type: 'Identifier', name });
 const wait = argument => ({ type: 'AwaitExpression', argument });
 const spread = argument => ({ type: 'SpreadElement', argument });
-const invoke = (callee, arguments) => wait({ type: 'CallExpression', callee, arguments });
-// TODO: only wrap invocation in await if in async context
-
-const conditional = (statement, test, consequent, alternative) =>
-  ({ type: statement ? 'IfStatement' : 'ConditionalExpression', test, consequent, alternative });
+const block = (body, statement = false) => statement ? { type: 'BlockStatement', body } : { type: 'SequenceExpression', expressions: body };
+const arrow = (params, body, expression = true, async = false) => ({ type: 'ArrowFunctionExpression', async, expression, params, body });
+const invoke = (callee, arguments, async = false) => (async ? nop : wait)({ type: 'CallExpression', callee, arguments });
+const conditional = (test, consequent, alternative, statement = false) => ({ type: statement ? 'IfStatement' : 'ConditionalExpression', test, consequent, alternative });
 const logical = (operator, left, right) => ({ type: 'LogicalExpression', operator, left, right });
-const attempt = (block, param, body) => ({ type: 'TryStatement', block, handler: { type: 'CatchClause', param, body } });
+const ifee = expr => invoke(arrow([], block([expr], true)), []);
+const attempt = (block, param, body, statement = false) => (statement ? nop : ifee)({ type: 'TryStatement', block, handler: { type: 'CatchClause', param, body } });
 const access = (object, property) => ({ type: 'MemberExpression', computed: property.type !== 'Identifier', object, property });
 const assign = (left, right, operator = '=') => ({ type: 'AssignmentExpression', left, right, operator });
-const block = (statement, body) => statement ? { type: 'BlockStatement', body } : { type: 'SequenceExpression', expressions: body };
 const statement = expression => ({ type: 'ExpressionStatement', expression });
-const arrow = (params, body, expression = true) => ({ type: 'ArrowFunctionExpression', async: true, expression, params, body });
-// TODO: track async in context, and should async always be there for generated code?
 
+const tag = (x, name, value) => (x[name] = value, x);
+const but = (x, name, value) => ({ ...x, [name]: value });
+const butLocals = (x, locals) => ({ ...x, locals });
+const addLocals = (x, locals) => ({ ...x, locals: [...x.locals, ...locals] });
 const ensure = (kind, expr) => expr.kind === kind ? expr : invoke(identifier('as' + kind), [expr]);
 
-const but = (x, name, value) => ({ ...x, [name]: value });
-const withLocals = (x, locals) => ({ ...x, locals: [...x.locals, ...locals] });
+const inStatement = x => ({ ...x, statement: true, expression: false });
+const inExpression = x => ({ ...x, statement: false, expression: true });
+const inReturn = x => ({ ...x, statement: false, expression: true, return0: true });
+const inHead = x => ({ ...x, head: true, tail: false });
+const inTail = x => ({ ...x, head: false, tail: true });
+const inKind = (kind, x) => ({ ...x, kind });
 
 const isForm = (expr, lead, length) =>
   (!length || expr.length === length || raise(`${lead} must have ${length - 1} argument forms`))
   && expr[0] === intern(lead);
 const flattenForm = (expr, lead) => isForm(expr, lead) ? expr.slice(1).flatMap(x => flattenForm(x, lead)) : [expr];
 const flattenLogicalForm = (context, expr, lead) =>
-  flattenForm(expr, lead)
-    .map(x => build(but(context, 'kind', 'JsBool'), x)) // TODO: wrap in asJsBool if necessary
-    .reduceRight((right, left) => logical(lead === 'and' ? '&&' : '||', left, right)); // TODO: wrap in asKlBool if necessary
+  ensure(
+    'ShenBool',
+    flattenForm(expr, lead)
+      .map(x => ensure('JsBool', build(inHead(inKind('JsBool', context)), x)))
+      .reduceRight((right, left) => logical(lead === 'and' ? '&&' : '||', left, right)));
 
 const isReferenced = (symbol, expr) =>
   expr === symbol
@@ -140,81 +136,90 @@ const isReferenced = (symbol, expr) =>
 
 const hex = ch => ('0' + ch.charCodeAt(0).toString(16)).slice(-2);
 
-const validCharacter = ch => ch >= 'A' && ch <= 'Z' || ch >= 'a' && ch <= 'z' || ch >= 0 && ch <= 9 || ch === '_' || ch === '$';
-const validCharactersRegex = /[_$A-Za-z][_$A-Za-z0-9]*/;
+const validCharacterRegex = /^[_$A-Za-z0-9]$/;
+const validCharactersRegex = /^[_$A-Za-z][_$A-Za-z0-9]*$/;
 const validIdentifier = s => validCharactersRegex.test(s);
-const escapeCharacter = ch => validCharacter(ch) ? ch : ch === '-' ? '_' : `$${hex(ch)}`;
-const escapeIdentifier = s => identifier(s.split('').map(escapeCharacter).join(''));
+const escapeCharacter = ch => validCharacterRegex.test(ch) ? ch : ch === '-' ? '_' : `$${hex(ch)}`;
+const escapeIdentifier = s => identifier(nameOf(s).split('').map(escapeCharacter).join(''));
 
-const idleSymbol = symbol => invoke(identifier('intern'), [literal(nameof(symbol))]);
+/* statement    if location in target context can be a statement               *
+ * expression   if location in target context must be an expression            *
+ * async        transpiler should generate async/await syntax                  *
+ * return       location is the last statement which needs to be returned      *
+ * assignment   expr is getting assigned to a variable                         *
+ * head         if context is in head position                                 *
+ * tail         if context is in tail position                                 *
+ * locals       Set of local variables and parameters defined at this point    *
+ * scopeName    Name of enclosing function/file                                *
+ * kind         specific type is expected for expression, undefined if unknown */
 
-const lookup = (namespace, name) => access(identifier(namespace), validIdentifier(name) ? identifier(name) : literal(name));
-
-// TODO: async/await
 // TODO: inlining, type-inferred optimizations
+//       context.kind signals expected, ast.kind signals actual
+
+const buildIdleSymbol = symbol => invoke(identifier('intern'), [literal(nameof(symbol))]);
+const buildLookup = (namespace, name) => access(identifier(namespace), validIdentifier(name) ? identifier(name) : literal(name));
+
+const buildKind(kind, context, expr) => ensure(kind, build(inKind(kind, context), expr));
 
 const build = (context, expr) =>
   isNull(expr) || isNumber(expr) || isString(expr) ? literal(expr) :
-  expr === shenTrue  ? identifier('shenTrue') :
-  expr === shenFalse ? identifier('shenFalse') :
-  isSymbol(expr) ? (context.locals.includes(expr) ? escapeIdentifier(nameOf(expr)) : idleSymbol(expr)) :
+  isSymbol(expr) ? (context.locals.includes(expr) ? escapeIdentifier(expr) : buildIdleSymbol(expr)) :
   isArray(expr) ? (
     isForm(expr, 'and') ? flattenLogicalForm(context, expr, 'and') :
     isForm(expr, 'or')  ? flattenLogicalForm(context, expr, 'or') :
     isForm(expr, 'if', 4) ?
       conditional(
-        context.statement,
-        build(but(context, 'kind', 'JsBool'), expr[1]),
-        build(context, expr[2]),
-        build(context, expr[3])) :
+        buildKind('JsBool', inHead(context), expr[1]),
+        build(inTail(context), expr[2]),
+        build(inTail(context), expr[3]),
+        context.statement) :
     isForm(expr, 'cond') ?
       // TODO: if cond is in return/expression position, wrap in ReturnStatement
       expr.slice(1).reduceRight(
         (chain, [test, consequent]) =>
           test === shenTrue ? build(context, consequent) :
           conditional(
-            context.statement,
-            build(but(context, 'kind', 'JsBool'), test), // TODO: need 1 function that sets kind on context and does ensure()
+            buildKind('JsBool', inHead(context), test),
             build(context, consequent),
-            chain),
+            chain,
+            context.statement),
         invoke(identifier('raise'), [literal('no condition was true')])) :
     isForm(expr, 'let', 4) ? (
       // TODO: in a statement context, we can just add a declaration, maybe surround in a block
       // in an expression context, we might have to put it in an ifee, or attempt inlining
       nameOf(expr[1]) === '_' || !isReferenced(expr[1], expr[3])
         ? build(context, [intern('do'), expr[2], expr[3]]) // (let _ X Y) => (do X Y)
-        : statement(invoke(arrow([identifier(nameOf(expr[1]))], build(context, expr[2])), [build(context, expr[3])]))) :
-    isForm(expr, 'do') ? block(context.statement, flattenForm(expr, 'do').map(x => build(context, x))) :
+        // TODO: do an actual const declaration
+        : statement(invoke(
+            arrow([identifier(nameOf(expr[1]))], build(inHead(context), expr[2])),
+            [build(addLocals(context, [expr[1]]), expr[3])]))) :
+    isForm(expr, 'do') ? block(flattenForm(expr, 'do').map(x => build(context, x)), context.statement) :
     // TODO: return butlast(exprs)
-    // if do is assigned to a variable, just make last line an assignment to a variable
-    isForm(expr, 'lambda', 3) ? arrow([identifier(nameOf(expr[1]))], build(context, expr[2])) :
+    // TODO: if do is assigned to a variable, just make last line an assignment to a variable
+    isForm(expr, 'lambda', 3) ? arrow([identifier(nameOf(expr[1]))], build(addLocals(context, [expr[1]]), expr[2])) :
     // TODO: check body to see if function should be a statement or expression lambda
     isForm(expr, 'freeze', 2) ? arrow([], build(context, expr[1])) :
     // TODO: wrap in block statement, make it statement context
     isForm(expr, 'trap-error', 3) ? (
-      isForm(expr[2], 'lambda', 2)
-        ? attempt(build(context, expr[1]), identifier(nameOf(expr[2][1])), build(but(context, 'statement', true), expr[2][2]))
+      isForm(expr, 'lambda', 2)
+        ? attempt(build(context, expr[1]), identifier(nameOf(expr[2][1])), build(inStatement(context), expr[2][2]))
         : attempt(build(context, expr[1]), identifier('$error'), invoke(build(context, expr[2]), [identifier('$error')]))) :
     isForm(expr, 'defun', 4) ?
-      block(
-        false,
-        [
-          assign(
-            lookup('functions', nameOf(expr[1])),
-            arrow(
-              expr[2].map(nameOf).map(escapeIdentifier),
-              build(but(context, 'locals', expr[2].map(x => nameOf(x))), expr[3]))),
-          idleSymbol(expr[1])
-        ]) :
+      block([
+        assign(
+          buildLookup('functions', nameOf(expr[1])),
+          arrow(expr[2].map(escapeIdentifier), build(butLocals(inTail(context), expr[2].map(x => nameOf(x))), expr[3]))),
+        buildIdleSymbol(expr[1])]) :
     // TODO: value and set can be done as inlines
     // TODO: extract code for value to use in set
-    isForm(expr, 'value', 2) ? lookup('symbols', nameOf(expr[1])) :
-    isForm(expr, 'set', 3) ? assign(lookup('symbols', nameOf(expr[1])), build(context, expr[2])) :
+    //isForm(expr, 'value', 2) ? buildLookup('symbols', nameOf(expr[1])) :
+    //isForm(expr, 'set', 3) ? assign(buildLookup('symbols', nameOf(expr[1])), build(context, expr[2])) :
     // TODO: tag returned expr as having type nameof(expr[2])
-    isForm(expr, 'type', 2) ? expr[1] :
+    //isForm(expr, 'type', 2) ? expr[1] :
     // TODO: check inlines here
+    // TODO: bounce applications in tail position, settle applications in head position
     context.locals.includes(expr[0]) ? invoke(identifier(nameOf(expr[0])), expr.slice(1).map(x => build(context, x))) :
-    isSymbol(expr[0]) ? invoke(lookup('functions', nameOf(expr[0])), expr.slice(1).map(x => build(context, x))) :
+    isSymbol(expr[0]) ? invoke(buildLookup('functions', nameOf(expr[0])), expr.slice(1).map(x => build(context, x))) :
     raise('not a valid form')
   ) : raise('not a valid form');
 
@@ -253,22 +258,21 @@ const shenToJs = x =>
   x === false ? shenFalse :
   x;
 
-// TODO: include all functions needed by generated code in the object returned by kl
-// TODO: kl() ... translated code ... shen() ... shen.repl()       no constructors!
+// TODO: kl() ... translated code ... shen() ... shen.repl()
 
 export default (options = {}) => {
   const asInStream  = x => options.isInStream  && (options.isInStream(x)  ? x : raise('input stream expected'));
   const asOutStream = x => options.isOutStream && (options.isOutStream(x) ? x : raise('output stream expected'));
   const isStream = x => options.isInStream(x) || options.isOutStream(x);
   const asStream = x => isStream(x) ? x : raise('stream expected');
-  const clock = options.clock || () => new Date().getTime();
+  const clock = options.clock || (() => new Date().getTime());
   const startTime = clock();
   const getTime = mode =>
     mode === 'unix' ? clock() :
     mode === 'run'  ? clock() - startTime :
     raise(`get-time only accepts symbols unix or run, not ${mode}`);
-  const openRead  = options.openRead  || () => raise('open(in) not supported');
-  const openWrite = options.openWrite || () => raise('open(out) not supported');
+  const openRead  = options.openRead  || (() => raise('open(in) not supported'));
+  const openWrite = options.openWrite || (() => raise('open(out) not supported'));
   const open = (mode, path) =>
     mode === 'in'  ? openRead(path) :
     mode === 'out' ? openWrite(path) :
@@ -294,15 +298,16 @@ export default (options = {}) => {
     '*os*':             options.os             || 'Unknown',
     '*port*':           options.port           || 'Unknown',
     '*porters*':        options.porters        || 'Unknown',
-    '*stinput*':        options.stinput        || () => raise('standard input not supported'),
-    '*stoutput*':       options.stoutput       || () => raise('standard output not supported'),
-    '*sterror*':        options.sterror        || () => raise('standard error not supported'),
-    '*home-directory*': options.homeDirectory  || '';
+    '*stinput*':        options.stinput        || (() => raise('standard input not supported')),
+    '*stoutput*':       options.stoutput       || (() => raise('standard output not supported')),
+    '*sterror*':        options.sterror        || (() => raise('standard error not supported')),
+    '*home-directory*': options.homeDirectory  || ''
   };
   const context = Object.freeze({
     locals: new Set(),
     head: true,
-    useAsync: options.useAsync
+    expression: true,
+    async: options.async
   });
   const functions = {
     'if':              (b, x, y) => asJsBool(b) ? x : y,
@@ -327,8 +332,8 @@ export default (options = {}) => {
     'pos':             (s, i) => asString(s)[asNumber(i)],
     'str':             show,
     'absvector':       n => new Array(asNumber(n)).fill(null),
-    '<-absvector':     (a, i) => asArray(a)[asIndex(i, a)],
-    'absvector->':     (a, i, x) => (asArray(a)[asIndex(i, a)] = x, a),
+    '<-address':       (a, i) => asArray(a)[asIndex(i, a)],
+    'address->':       (a, i, x) => (asArray(a)[asIndex(i, a)] = x, a),
     '+':               (x, y) => asNumber(x) + asNumber(y),
     '-':               (x, y) => asNumber(x) - asNumber(y),
     '*':               (x, y) => asNumber(x) * asNumber(y),

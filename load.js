094f35fa2dba91dd5cf0a7b815b57e3b356d0ca2
Robert Koeninger <rkoeninger@att.net>
2019-02-14 19:44:28 -0500

    Cleaup parser, load

--- load.js
+++ load.js
@@ -1,24 +1,11 @@
+const fs = require('fs');
 const { kl } = require('./refactor/core.js');
 const { parse } = require('./parser.js');
 
 const files = [
-  'toplevel',
-  'core',
-  'sys',
-  'dict',
-  'sequent',
-  'yacc',
-  'reader',
-  'prolog',
-  'track',
-  'load',
-  'writer',
-  'macros',
-  'declarations',
-  'types',
-  't-star'
+  'toplevel', 'core',   'sys',          'dict',  'sequent',
+  'yacc',     'reader', 'prolog',       'track', 'load',
+  'writer',   'macros', 'declarations', 'types', 't-star'
 ];
 
-const fs = require('fs');
-
-files.forEach(file => console.log(parse(fs.readFileSync(`./kernel/klambda/${file}.kl`, 'utf-8'))));
+files.forEach(file => console.log(parse(fs.readFileSync(`./kernel/klambda/${file}.kl`, 'utf-8')).length));
--- parser.js
+++ parser.js
@@ -1,12 +1,12 @@
 const { alt, createLanguage, regexp, string } = require('parsimmon');
 
 const language = createLanguage({
-  whitespace: _ => regexp(/\s*/m).desc('whitespace'),
-  numeric:    _ => regexp(/-?\d+/).map(Number).desc('numeric'),
-  textual:    _ => regexp(/[^"]*/m).wrap(string('"'), string('"')).desc('textual'),
-  symbolic:   _ => regexp(/[\w\-\+=*&^%$#@!~`<>,\?\./\\;:{}|]+/).map(Symbol.for).desc('symbolic'),
-  value:      r => alt(r.numeric, r.textual, r.symbolic, r.form).desc('value'),
-  form:       r => r.value.trim(r.whitespace).many().wrap(string('('), string(')')).desc('form'),
-  file:       r => r.value.trim(r.whitespace).many().desc('file')
+  whitespace: _ => regexp(/\s*/m),
+  numeric:    _ => regexp(/-?\d+/).map(Number),
+  textual:    _ => regexp(/[^"]*/m).trim(string('"')),
+  symbolic:   _ => regexp(/[\w\-\+=*&^%$#@!~`,;:|<>{}\[\]\?\.\\/]+/).map(Symbol.for),
+  value:      r => alt(r.numeric, r.textual, r.symbolic, r.form),
+  form:       r => r.value.trim(r.whitespace).many().wrap(string('('), string(')')),
+  file:       r => r.value.trim(r.whitespace).many()
 });
 exports.parse = s => language.file.tryParse(s);

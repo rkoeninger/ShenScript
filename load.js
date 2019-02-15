1e5da0177a33e5f139b09ee3b1cd3edcd27b441a
Robert Koeninger <rkoeninger@att.net>
2019-02-15 16:57:29 -0500

    Split defuns and setups in load.js

--- load.js
+++ load.js
@@ -8,4 +8,29 @@ const files = [
   'writer',   'macros', 'declarations', 'types', 't-star'
 ];
 
-files.forEach(file => console.log(parse(fs.readFileSync(`./kernel/klambda/${file}.kl`, 'utf-8')).length));
+const env = kl();
+const { cons, symbolOf, valueToArrayTree } = env;
+const evalKl = env.functions['eval-kl'];
+const defuns = [];
+const setups = [];
+
+files.forEach(file =>
+  parse(fs.readFileSync(`./kernel/klambda/${file}.kl`, 'utf-8')).forEach(expr =>
+    (expr[0] === symbolOf('defun') ? defuns : setups).push(expr)));
+
+const load = expr => {
+  try {
+    evalKl(expr);
+  } catch (e) {
+    console.log('--------------------------------------------------------------------------------');
+    console.log(expr);
+    console.log(e);
+  }
+};
+
+console.log('================================================================================');
+console.log('defuns...');
+defuns.forEach(load);
+console.log('================================================================================');
+console.log('setups...');
+setups.forEach(load);

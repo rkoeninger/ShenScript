a37ab63b027c64025568c87b7e817e5f058e2184
Robert Koeninger <rkoeninger@att.net>
2019-02-25 01:47:43 -0500

    Made temp testing changes to load.js

--- load.js
+++ load.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const { generate } = require('astring');
 const { kl } = require('./refactor/core.js');
 const { parse } = require('./parser.js');
 
@@ -13,17 +14,18 @@ const { cons, symbolOf, valueToArrayTree } = env;
 const evalKl = env.functions['eval-kl'];
 const defuns = [];
 const setups = [];
+const debris = [];
 
 files.forEach(file =>
   parse(fs.readFileSync(`./kernel/klambda/${file}.kl`, 'utf-8')).forEach(expr =>
-    (expr[0] === symbolOf('defun') ? defuns : setups).push(expr)));
+    (!Array.isArray(expr) ? debris : expr[0] === symbolOf('defun') ? defuns : setups).push(expr)));
 
 const load = expr => {
   try {
     evalKl(expr);
   } catch (e) {
     console.log('--------------------------------------------------------------------------------');
-    console.log(expr);
+    console.log(generate(env.build({ locals: new Set(), head: true, expression: true }, expr)));
     console.log(e);
   }
 };
@@ -34,3 +36,6 @@ defuns.forEach(load);
 console.log('================================================================================');
 console.log('setups...');
 setups.forEach(load);
+
+// const text = '(defun shen.dict (V3255) (cond ((< V3255 1) (simple-error (cn "invalid initial dict size: " (shen.app V3255 "" shen.s)))) (true (let D (absvector (+ 3 V3255)) (let Tag (address-> D 0 shen.dictionary) (let Capacity (address-> D 1 V3255) (let Count (address-> D 2 0) (let Fill (shen.fillvector D 3 (+ 2 V3255) ()) D))))))))';
+// console.log(generate(env.build({ locals: new Set(), head: true, expression: true }, parse(text)[0])));

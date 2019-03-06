2c6e036a2fe3a5d93f3ab0a31a1ba4ea72646725
Robert Koeninger <rkoeninger@att.net>
2019-03-05 23:40:29 -0500

    Cleanup load.js

--- load.js
+++ load.js
@@ -3,39 +3,13 @@ const { generate } = require('astring');
 const { kl } = require('./refactor/core.js');
 const { parse } = require('./parser.js');
 const env = kl();
-const defuns = [];
-const setups = [];
-const debris = [];
 
-const load = expr => {
-  try {
-    env.evalKl(env.context, env, expr);
-    if (expr[1] === env.symbolOf('shen.walk')) {
-      console.log(expr);
-      console.log(env.build(env.context, expr));
-      console.log(generate(env.build(env.context, expr)));
-    }
-  } catch (e) {
-    // console.log('--------------------------------------------------------------------------------');
-    // const ast = env.build(env.context, expr);
-    // console.log(ast);
-    // console.log(generate(ast));
-    // console.log(e);
-  }
-};
+const load = expr => env.trap(() => env.evalKl(env.context, env, expr), x => console.log(x));
 
-[
+const files = [
   'toplevel', 'core',   'sys',          'dict',  'sequent',
   'yacc',     'reader', 'prolog',       'track', 'load',
   'writer',   'macros', 'declarations', 'types', 't-star'
-].forEach(file =>
-  parse(fs.readFileSync(`./kernel/klambda/${file}.kl`, 'utf-8')).forEach(expr =>
-    (!Array.isArray(expr) ? debris : expr[0] === env.symbolOf('defun') ? defuns : setups).push(expr)));
+];
 
-defuns.forEach(load);
-setups.forEach(load);
-
-// console.log(env.symbols['shen.*demodulation-function*']);
-
-console.log(env.functions.eval(env.settle(env.functions['read-from-string']('(+ 1 2)')).head));
-console.log(env.settle(env.functions.eval(env.settle(env.functions['read-from-string']('(+ 1 2)')).head)));
+files.forEach(file => parse(fs.readFileSync(`./kernel/klambda/${file}.kl`, 'utf-8')).forEach(load));

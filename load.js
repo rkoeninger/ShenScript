7485210a329af40b89c29eccd65b1bbe72bbad50
Robert Koeninger <rkoeninger@att.net>
2019-03-06 17:40:25 -0500

    Added intercept shen.initialise_arity_table temporary optimization

--- load.js
+++ load.js
@@ -5,10 +5,23 @@ const { parse } = require('./parser.js');
 const env = kl();
 const load = expr =>
   env.trap(
-    () => env.evalKl(env.context, env, expr),
-    x => {
-      console.log(expr[0] === env.symbolOf('defun') || expr[0] === env.symbolOf('declare') ? expr[0] + ' ' + expr[1] : expr);
-      console.log(x);
+    () => {
+      if (Array.isArray(expr) && expr[0] === env.symbolOf('shen.initialise_arity_table')) {
+        // TODO: stack overflow caused just by building cons chain
+        console.log('intercepting shen.initialise_arity_table');
+        const aritySymbol = env.symbolOf('arity');
+        let x = expr[1];
+        while (Array.isArray(x) && x.length > 0) {
+          env.settle(env.functions.put(x[1], aritySymbol, x[2][1]));
+          x = x[2][2];
+        }
+      } else {
+        env.evalKl(env.context, env, expr);
+      }
+    },
+    e => {
+      console.log(expr[0] === env.symbolOf('defun') || expr[0] === env.symbolOf('declare') ? `${expr[0]} ${expr[1]}` : expr);
+      console.log(e);
     });
 
 const files = [

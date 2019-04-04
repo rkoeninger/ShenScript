c65d84b6a3e8d1b55511415f5968c8a9acaae5f7
Robert Koeninger <rkoeninger@att.net>
2019-04-03 21:44:04 -0400

    Added better time formatting to load.js

--- load.js
+++ load.js
@@ -82,18 +82,28 @@ const loadGroupAsync = async (name, exprs) => {
   console.log(`${name}: ${exprs.length} loaded in ${Date.now() - start}ms`);
 };
 
+const formatDuration = x =>
+  [[x / 60000, 'm'], [x / 1000 % 60, 's'], [x % 1000, 'ms']]
+    .filter(([n, _]) => n >= 1)
+    .map(([n, l]) => `${Math.floor(n)}${l}`)
+    .join(', ');
+
 if (async) {
   (async () => {
+    const start = Date.now();
     await loadGroupAsync('defuns', defuns);
     await loadGroupAsync('statements', statements);
     console.log(await evalKl([s`cd`, './kernel/tests']));
     console.log(await evalKl([s`load`, 'README.shen']));
     console.log(await evalKl([s`load`, 'tests.shen']));
+    console.log(`total time elapsed: ${formatDuration(Date.now() - start)}`);
   })();
 } else {
+  const start = Date.now();
   loadGroup('defuns', defuns);
   loadGroup('statements', statements);
   console.log(evalKl([s`cd`, './kernel/tests']));
   console.log(evalKl([s`load`, 'README.shen']));
   console.log(evalKl([s`load`, 'tests.shen']));
+  console.log(`total time elapsed: ${formatDuration(Date.now() - start)}`);
 }

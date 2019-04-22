b6c3894d05efb8bd18caadace6a49f44d80c596d
Robert Koeninger <rkoeninger@att.net>
2019-04-22 01:05:27 -0400

    Tried to get async stream working with awaitify-stream

---
+++ scripts/repl.js
@@ -0,0 +1,100 @@
+const fs = require('fs');
+const { addAsyncFunctions } = require('awaitify-stream');
+const backend = require('../src/backend');
+const { parse } = require('../scripts/parser');
+
+const InStream = class {
+  constructor() {
+    this.stream = addAsyncFunctions(process.stdin);
+  }
+  async read() {
+    const b = await this.stream.readAsync();
+    return b === null ? -1 : b;
+  }
+};
+
+(async () => {
+  const stream = new InStream();
+  let b = -1;
+  while ((b = await stream.read()) >= 0) {
+    console.log('read: ' + b);
+  }
+  console.log('end of input');
+})().then(_ => console.log('done'));
+
+// const OutStream = class {
+//   write(b) { return process.stdout.write(String.fromCharCode(b)); }
+// };
+
+// const InFileStream = class {
+//   constructor(buf) {
+//     this.buf = buf;
+//     this.pos = 0;
+//   }
+//   read() { return this.pos >= this.buf.length ? -1 : this.buf[this.pos++]; }
+//   close() {}
+// };
+
+// const OutFileStream = class {
+//   constructor(stream) { this.stream = stream; }
+//   write(b) { return this.stream.write(String.fromCharCode(b)); }
+//   close() { return this.stream.close(); }
+// };
+
+// const stinput = new InStream();
+// const stoutput = new OutStream();
+
+// let home = () => '';
+// const $ = backend({
+//   async: true,
+//   implementation: 'Node.js',
+//   release: process.version.slice(1),
+//   os: process.platform,
+//   port: '0.1.0',
+//   porters: 'Robert Koeninger',
+//   openRead: path => new InFileStream(fs.readFileSync(home() + path)),
+//   openWrite: path => new OutFileStream(fs.createWriteStream(home() + path, { flags: 'w' })),
+//   isInStream: x => x instanceof InStream || x instanceof InFileStream,
+//   isOutStream: x => x instanceof OutStream || x instanceof OutFileStream,
+//   stinput,
+//   stoutput,
+//   sterror: stoutput
+// });
+// const { evalKl, symbols, s } = $;
+// home = () => symbols['*home-directory*'];
+
+// const files = [
+//   'toplevel', 'core',   'sys',          'dict',  'sequent',
+//   'yacc',     'reader', 'prolog',       'track', 'load',
+//   'writer',   'macros', 'declarations', 'types', 't-star'
+// ];
+
+// const defuns = [];
+// const statements = [];
+
+// files.forEach(file => parse(fs.readFileSync(`./kernel/klambda/${file}.kl`, 'utf-8')).forEach(expr => {
+//   if (Array.isArray(expr) && expr.length > 0) {
+//     (expr[0] === s`defun` ? defuns : statements).push(expr);
+//   }
+// }));
+
+// const loadGroupAsync = async (name, exprs) => {
+//   const start = Date.now();
+//   let i = 0;
+//   console.log(`${name}: loading...`);
+//   for (let expr of exprs) {
+//     try {
+//       await evalKl(expr);
+//     } catch (e) {
+//       console.log(expr);
+//       console.log(e);
+//     }
+//   }
+//   console.log(`${name}: ${exprs.length} loaded in ${Date.now() - start}ms`);
+// };
+
+// (async () => {
+//   await loadGroupAsync('defuns', defuns);
+//   await loadGroupAsync('statements', statements);
+//   await evalKl([s`shen.shen`]);
+// })();

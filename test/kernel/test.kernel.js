916d0ebcc89764847b5802c25a3c95814422132d
Robert Koeninger <rkoeninger@att.net>
2019-04-23 14:36:09 -0400

    Simplified loading kernel in test.kernel

--- test/kernel/test.kernel.js
+++ test/kernel/test.kernel.js
@@ -33,25 +33,17 @@ const { evalKl, s } = backend({
 
 const { defuns, statements } = parseKernel();
 
-const loadGroup = (name, exprs) => {
-  const start = Date.now();
-  let i = 0;
-  console.log(`${name}: loading...`);
-  for (let expr of exprs) {
-    try {
-      evalKl(expr);
-    } catch (e) {
-      console.log(expr);
-      console.log(e);
-    }
-  }
-  console.log(`${name}: ${exprs.length} loaded in ${Date.now() - start}ms`);
-};
+const formatDuration = x =>
+  [[x / 60000, 'm'], [x / 1000 % 60, 's'], [x % 1000, 'ms']]
+    .filter(([n, _]) => n >= 1)
+    .map(([n, l]) => `${Math.floor(n)}${l}`)
+    .join(', ');
 
-const loadGroupAsync = async (name, exprs) => {
+(async () => {
   const start = Date.now();
   let i = 0;
-  console.log(`${name}: loading...`);
+  console.log(`loading kernel...`);
+  const exprs = [...defuns, ...statements];
   for (let expr of exprs) {
     try {
       await evalKl(expr);
@@ -60,31 +52,9 @@ const loadGroupAsync = async (name, exprs) => {
       console.log(e);
     }
   }
-  console.log(`${name}: ${exprs.length} loaded in ${Date.now() - start}ms`);
-};
-
-const formatDuration = x =>
-  [[x / 60000, 'm'], [x / 1000 % 60, 's'], [x % 1000, 'ms']]
-    .filter(([n, _]) => n >= 1)
-    .map(([n, l]) => `${Math.floor(n)}${l}`)
-    .join(', ');
-
-if (async) {
-  (async () => {
-    const start = Date.now();
-    await loadGroupAsync('defuns', defuns);
-    await loadGroupAsync('statements', statements);
-    console.log(await evalKl([s`cd`, config.testsPath]));
-    console.log(await evalKl([s`load`, 'README.shen']));
-    console.log(await evalKl([s`load`, 'tests.shen']));
-    console.log(`total time elapsed: ${formatDuration(Date.now() - start)}`);
-  })();
-} else {
-  const start = Date.now();
-  loadGroup('defuns', defuns);
-  loadGroup('statements', statements);
-  console.log(evalKl([s`cd`, config.testsPath]));
-  console.log(evalKl([s`load`, 'README.shen']));
-  console.log(evalKl([s`load`, 'tests.shen']));
+  console.log(`kernel (${exprs.length} exprs) loaded: ${formatDuration(Date.now() - start)}`);
+  console.log(await evalKl([s`cd`, config.testsPath]));
+  console.log(await evalKl([s`load`, 'README.shen']));
+  console.log(await evalKl([s`load`, 'tests.shen']));
   console.log(`total time elapsed: ${formatDuration(Date.now() - start)}`);
-}
+})();

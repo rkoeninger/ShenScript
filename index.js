0807ec7ebbf627bfe8056947d188757b36d1031d
Robert Koeninger <rkoeninger@att.net>
2019-05-01 16:52:34 -0400

    Added js.fetch to frontend.web when in async mode

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
+const async = true;
+
 const { top }  = require('./lib/utils');
 const config   = require('./lib/config.web');
 const backend  = require('./lib/backend');
-const kernel   = require('./dist/kernel.sync');
+const kernel   = require(`./dist/kernel.${async ? 'async' : 'sync'}`);
 const frontend = require('./lib/frontend.web');
 
-const options = { ...config };
+const options = { ...config, async };
 
 (async () => {
   try {
+    const start = new Date().getTime();
     console.log('creating shen environment...');
     top.shen = frontend(await kernel(backend(options)));
-    console.log('environment created.');
+    const end = new Date().getTime();
+    console.log(`environment created in ${end - start}ms.`);
   } catch (e) {
     console.error(e);
   }

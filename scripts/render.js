dd137aff23e9c42c7258662ecc2bbf170be010eb
Robert Koeninger <rkoeninger@att.net>
2019-06-10 23:08:23 -0400

    render.js now always generates sync and async kernels

--- scripts/render.js
+++ scripts/render.js
@@ -1,5 +1,3 @@
-const async = process.argv.includes('async');
-
 const fs              = require('fs');
 const { parseKernel } = require('./parser');
 const backend         = require('../lib/backend');
@@ -10,24 +8,29 @@ const {
 const { flatMap } = require('../lib/utils');
 
 const { defuns, statements } = parseKernel();
-const { compile } = backend({ async });
 
-const toStatements = fab => [...fab.statements, Statement(fab.expression)];
-const syntax =
-  generate(Program([Statement(Assign(
-    Member(Identifier('module'), Identifier('exports')),
-    Arrow(
-      [RawIdentifier('$')],
-      Block(
-        // TODO: top-level defuns and statements all start in ignore situation
-        ...flatMap([...defuns, ...statements], x => toStatements(compile(x))),
-        Return(RawIdentifier('$'))),
-      async)))]), { indent: '  ' }); // TODO: try to render each expr on a single line if possible
+const render = async => {
+  const { compile } = backend({ async });
+  const toStatements = fab => [...fab.statements, Statement(fab.expression)];
+  const syntax =
+    generate(Program([Statement(Assign(
+      Member(Identifier('module'), Identifier('exports')),
+      Arrow(
+        [RawIdentifier('$')],
+        Block(
+          // TODO: top-level defuns and statements all start in ignore situation
+          ...flatMap([...defuns, ...statements], x => toStatements(compile(x))),
+          Return(RawIdentifier('$'))),
+        async)))]), { indent: '  ' }); // TODO: try to render each expr on a single line if possible
 
-console.log(`${syntax.length} chars`);
+  console.log(`${async ? 'async' : 'sync '} kernel: ${syntax.length} chars`);
 
-if (!fs.existsSync('dist')) {
-  fs.mkdirSync('dist');
-}
+  if (!fs.existsSync('dist')) {
+    fs.mkdirSync('dist');
+  }
 
-fs.writeFileSync(`dist/kernel.${async ? 'async' : 'sync'}.js`, syntax);
+  fs.writeFileSync(`dist/kernel.${async ? 'async' : 'sync'}.js`, syntax);
+};
+
+render(false);
+render(true);

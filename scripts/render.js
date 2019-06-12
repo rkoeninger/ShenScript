4557271bb98e7cdbaa49f8860cdeaf4942032ed3
Robert Koeninger <rkoeninger@att.net>
2019-06-12 00:38:30 -0400

    Renamed RawId to Id

--- scripts/render.js
+++ scripts/render.js
@@ -2,25 +2,21 @@ const fs              = require('fs');
 const { parseKernel } = require('./parser');
 const backend         = require('../lib/backend');
 const { flatMap }     = require('../lib/utils');
-const {
-  Arrow, Assign, Block, Member, Program, RawId, Return, Statement,
-  generate
-} = require('../lib/ast');
+const { Arrow, Assign, Async, Block, Id, Member, Program, Return, Statement, generate } = require('../lib/ast');
 const { defuns, statements } = parseKernel();
+const exprs = [...defuns, ...statements];
 
 const render = async => {
   const { compile } = backend({ async });
-  const toStatements = fab => [...fab.statements, Statement(fab.expression)];
-  const syntax =
-    generate(Program([Statement(Assign(
-      Member(RawId('module'), RawId('exports')),
-      Arrow(
-        [RawId('$')],
+  const syntax = generate(
+    Program([Statement(Assign(
+      Member(Id('module'), Id('exports')),
+      Async(Arrow([Id('$')],
         Block(
           // TODO: top-level defuns and statements all start in ignore situation
-          ...flatMap([...defuns, ...statements], x => toStatements(compile(x))),
-          Return(RawId('$'))),
-        async)))]), { indent: '  ' }); // TODO: try to render each expr on a single line if possible
+          ...flatMap(exprs.map(compile), fabr => [...fabr.statements, Statement(fabr.expression)]),
+          Return(Id('$')))))))]),
+    { indent: '  ' }); // TODO: try to render each expr on a single line if possible
 
   console.log(`${async ? 'async' : 'sync '} kernel: ${syntax.length} chars`);
 

65e23f4d0d98859785bd1deb8ee8330a6832d491
Robert Koeninger <rkoeninger@att.net>
2019-06-12 01:12:54 -0400

    Fixed sync kernel rendering
    Fixed js.super using non-existent Call$

--- scripts/render.js
+++ scripts/render.js
@@ -2,7 +2,7 @@ const fs              = require('fs');
 const { parseKernel } = require('./parser');
 const backend         = require('../lib/backend');
 const { flatMap }     = require('../lib/utils');
-const { Arrow, Assign, Async, Block, Id, Member, Program, Return, Statement, generate } = require('../lib/ast');
+const { Arrow, Assign, Block, Id, Member, Program, Return, Statement, generate } = require('../lib/ast');
 const { defuns, statements } = parseKernel();
 const exprs = [...defuns, ...statements];
 
@@ -11,11 +11,13 @@ const render = async => {
   const syntax = generate(
     Program([Statement(Assign(
       Member(Id('module'), Id('exports')),
-      Async(Arrow([Id('$')],
+      Arrow(
+        [Id('$')],
         Block(
           // TODO: top-level defuns and statements all start in ignore situation
           ...flatMap(exprs.map(compile), fabr => [...fabr.statements, Statement(fabr.expression)]),
-          Return(Id('$')))))))]),
+          Return(Id('$'))),
+        async)))]),
     { indent: '  ' }); // TODO: try to render each expr on a single line if possible
 
   console.log(`${async ? 'async' : 'sync '} kernel: ${syntax.length} chars`);

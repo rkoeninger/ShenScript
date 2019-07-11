69b0a8fbd2063ae100963a25c358d7dc30ee3e64
Robert Koeninger <rkoeninger@att.net>
2019-07-11 15:28:37 -0400

    Set docs theme, logo

--- docs/conf.py
+++ docs/conf.py
@@ -38,15 +38,6 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'alabaster'
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
+html_theme = 'sphinx_rtd_theme'
+html_logo = '../assets/logo.svg'
 html_static_path = ['_static']

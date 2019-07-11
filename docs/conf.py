a0134aa4c6eac16fb740f7d557d03504f8e59964
Robert Koeninger <rkoeninger@att.net>
2019-07-11 15:31:02 -0400

    Cleanup docs config

--- docs/conf.py
+++ docs/conf.py
@@ -1,43 +1,11 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# http://www.sphinx-doc.org/en/master/config
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
-
-# -- Project information -----------------------------------------------------
+# For config options, refer to: http://www.sphinx-doc.org/en/master/config
 
 project = 'ShenScript'
 copyright = '2019, Robert Koeninger'
 author = 'Robert Koeninger'
-
-
-# -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-extensions = [
-]
-
-# Add any paths that contain templates here, relative to this directory.
+extensions = []
 templates_path = ['_templates']
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
 html_theme = 'sphinx_rtd_theme'
 html_logo = '../assets/logo.svg'
 html_static_path = ['_static']

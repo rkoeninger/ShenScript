ffc7f65bb82c48e0ba44d3b316466a0c2893aacc
Robert Koeninger <rkoeninger@att.net>
2019-07-16 20:24:16 -0400

    Added master_doc='index' to docs config

--- docs/conf.py
+++ docs/conf.py
@@ -1,11 +1,12 @@
 # For config options, refer to: http://www.sphinx-doc.org/en/master/config
 
 project = 'ShenScript'
-copyright = '2019, Robert Koeninger'
 author = 'Robert Koeninger'
+copyright = '2019, ' + author
 extensions = []
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 html_theme = 'sphinx_rtd_theme'
 html_logo = '../assets/logo.svg'
 html_static_path = ['_static']
+master_doc = 'index'

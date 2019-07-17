bf5e43a53b0f7d3eb6173937e9f7b49c44cf635b
Robert Koeninger <rkoeninger@att.net>
2019-07-17 17:15:34 -0400

    Added jsdoc, sphinx-js, jsdoc comments to backend, frontend

--- docs/conf.py
+++ docs/conf.py
@@ -3,7 +3,7 @@
 project = 'ShenScript'
 author = 'Robert Koeninger'
 copyright = '2019, ' + author
-extensions = []
+extensions = ['sphinx_js']
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 html_theme = 'sphinx_rtd_theme'

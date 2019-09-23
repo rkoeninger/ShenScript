0cc76db4e429f8da8a78b6861fd7a5061cb71258
Robert Koeninger <rkoeninger@att.net>
2019-09-23 15:16:10 -0400

    Added InStream based on fetch to index.js

--- index.js
+++ index.js
@@ -2,7 +2,16 @@
   try {
     const start = new Date().getTime();
     console.log('creating shen environment...');
-    window.shen = await require('./shen')({ async: true, target: 'web' });
+    const InStream = class {
+      constructor(text) {
+        this.text = text;
+        this.pos = 0;
+      }
+      read() { return this.pos >= this.text.length ? -1 : this.text.charCodeAt(this.pos++); }
+      close() { return (this.pos = Infinity, null); }
+    };
+    const openRead = path => fetch(path).then(x => x.text()).then(x => new InStream(x));
+    window.shen = await require('./shen')({ async: true, target: 'web', openRead, InStream });
     const end = new Date().getTime();
     const message = `environment created in ${end - start}ms.`;
     console.log(message);

8304f55506a334876c044d04ad550fe2db32e021
Robert Koeninger <rkoeninger@att.net>
2019-10-07 14:34:37 -0400

    shen module exports Shen class; dev index page styling moved to html

--- index.development.js
+++ index.development.js
@@ -1,4 +1,4 @@
-const shen = require('./lib/shen.js');
+const Shen = require('./lib/shen.js');
 const { StringInStream, onReady } = require('./lib/utils.js');
 
 (async () => {
@@ -9,29 +9,15 @@ const { StringInStream, onReady } = require('./lib/utils.js');
 
     const openRead = async path => new StringInStream(await (await fetch(path)).text());
     const options = { async: true, target: 'web', openRead, InStream: StringInStream };
-    window.shen = await shen(options);
+    window.$ = await new Shen(options);
     const message = () => `shen environment created in ${now() - start}ms.`;
 
     onReady(() => {
-      const text = document.createTextNode(message());
       const p = document.createElement('p');
-      Object.assign(p.style, {
-        color: '#323330',
-        backgroundColor: '#F0DB4F',
-        border: '0.25em solid #323330',
-        margin: '0.5em',
-        padding: '0.5em',
-        fontSize: '20px',
-        textAlign: 'center',
-        cursor: 'pointer',
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        right: 0
-      });
+      p.classList.add('notification');
+      p.append(document.createTextNode(message()));
       p.setAttribute('title', 'Click to dismiss');
       p.onclick = () => document.body.removeChild(p);
-      p.append(text);
       document.body.prepend(p);
     });
     console.log(message());
--- index.production.js
+++ index.production.js
@@ -1,6 +1,10 @@
-const shen = require('./lib/shen.js');
+const ShenBase = require('./lib/shen.js');
 const { StringInStream } = require('./lib/utils.js');
 
 const openRead = path => fetch(path).then(x => x.text()).then(x => new StringInStream(x));
-const options = { async: true, target: 'web', openRead, InStream: StringInStream };
-shen(options).then(x => window.shen = x, e => console.error(e));
+
+window.Shen = class extends ShenBase {
+  constructor(options) {
+    super({ async: true, target: 'web', openRead, InStream: StringInStream, ...options });
+  }
+};

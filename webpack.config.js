c6472c5701d98d748d2b70e0763c454524795750
Robert Koeninger <rkoeninger@att.net>
2019-10-08 17:50:42 -0400

    Added babel transform to remove use of obj spread
    Works in Edge now

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,20 @@ const path = require('path');
 module.exports = env => ({
   mode: env.mode,
   entry: `./index.${env.mode}.js`,
+  module: {
+    rules: [
+      {
+        test: x => !x.includes('kernel.'),
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: ['@babel/plugin-proposal-object-rest-spread']
+          }
+        }
+      }
+    ]
+  },
   output: {
     path: path.resolve(__dirname, 'dist/' + env.mode),
     filename: 'shenScript.js'
